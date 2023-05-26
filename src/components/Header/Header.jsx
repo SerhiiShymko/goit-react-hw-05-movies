906420372968b0b0893d359f86f4a96cf4e6120d
SerhiiShymko <shimko456@gmail.com>
2023-05-26 22:40:04 +0300

    header_css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,21 @@
 import { NavLink } from 'react-router-dom';
+import css from './Header.module.css';
 
 
 const Header = () => {
   return (
-    <nav className="">
-      <div className="container">
-        {/* <span className="navbar-brand">Navbar</span> */}
-        <div className="navbar-nav">
-          <NavLink className="nav-link" aria-current="page" to="/">
+    <div className={css.container}>
+      <div className={css.navbar}>
+        <nav className={css.nav}>
+          <NavLink className={css.navLink} aria-current="page" to="/">
             Home
           </NavLink>
-          <NavLink className="nav-link" to="/movies">
+          <NavLink className={css.navLink} to="/movies">
             Movies
           </NavLink>
-        </div>
+        </nav>
       </div>
-    </nav>
+    </div >
   );
 };
 
